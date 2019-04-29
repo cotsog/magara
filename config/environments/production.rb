5b8d06cfe530b47e13b45374f0e3d535a44632c6
Berkhan Berkdemir <24360355+BerkhanBerkdemir@users.noreply.github.com>
2019-04-29 14:59:40 -0700

    :ambulance: change to correct domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV['MAILGUN_API_KEY'],
-    domain: 'magara.ist'
+    domain: 'mg.magara.ist'
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
